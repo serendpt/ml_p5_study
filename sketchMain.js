4250098e4b88abe928264327e96657c0cbed0897
serendpt <31066329+serendpt@users.noreply.github.com>
2022-11-29 00:04:10 +0900

    Update sketchMain.js

--- sketchMain.js
+++ sketchMain.js
@@ -1,16 +1,21 @@
 function setup() {
-  createCanvas(400, 400);
-  background(220);
-  let tex = createP();
-  tex.style('font-size', '20px');
-  tex.position(60, 165);
-  katex.render('\\dot{\\theta}_{i} = \\omega_{i} + \\frac{K}{N}  \\displaystyle\\sum_{j=1}^N \\sin(\\theta_{j} - \\theta_{i})', tex.elt);
-  let label = createP('Kuramoto Model');
-  label.style('font-size', '20px');
-  label.style('font-family', 'KaTeX-Main');
-  label.position(135, 120);
-}
+  createCanvas(400, 400)
+  background(220)
 
-function draw(){
-  rect(30, 20, 55, 55);
 }
+let value
+function draw(){
+  rect(30, 20, 55, 55)
+  let tex = createP()
+  tex.style('font-size', '20px')
+  tex.position(60, 165)
+  katex.render('\\dot{\\theta}_{i} = \\omega_{i} + \\frac{K}{N}  \\displaystyle\\sum_{j=1}^N \\sin(\\theta_{j} - \\theta_{i})', tex.elt)
+  let label = createP('Kuramoto Model')
+  label.style('font-size', '120')
+  label.style('font-family', 'KaTeX-Main')
+  label.position(135, 120)
+  //label.mouseOver()
+}
+function mousePressed() {
+  
+  }
