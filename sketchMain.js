48dfd2ffeb6b173c35a8458d236537a09e92b115
serendpt <31066329+serendpt@users.noreply.github.com>
2022-11-28 23:26:34 +0900

    Add files via upload

---
+++ sketchMain.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400)
+  background(220)
+  let tex = createP()
+  tex.style('font-size', '20px')
+  tex.position(60, 165)
+  katex.render('\\dot{\\theta}_{i} = \\omega_{i} + \\frac{K}{N}  \\displaystyle\\sum_{j=1}^N \\sin(\\theta_{j} - \\theta_{i})', tex.elt)
+  let label = createP('Kuramoto Model')
+  label.style('font-size', '20px')
+  label.style('font-family', 'KaTeX-Main')
+  label.position(135, 120)
+}
+
+function draw(){
+  rect(30, 20, 55, 55)
+}
